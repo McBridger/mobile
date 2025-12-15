6f0837bbc293a1bc53599603925151eda4b14fa1
Andrii Gerasymenko <agressicum@gmail.com>
2025-12-15 19:39:08 +0100

    chore: init foreground service after enabling permissions

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
-import ConnectorModule, { useConnector } from "@/modules/connector";
+import { useConnector } from "@/modules/connector";
 import { Redirect } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 import { useBluetoothPermissions } from "../hooks/useBluetoothPermissions";
-import { useAppConfig } from "@/hooks/useConfig";
-import { useEffect } from "react";
 
 // AppRegistry.registerHeadlessTask(
 //   BridgerHeadlessTask.name,
@@ -11,14 +9,8 @@ import { useEffect } from "react";
 // );
 
 export default function AppEntry() {
-  const { extra } = useAppConfig();
   const { isLoading, allPermissionsGranted } = useBluetoothPermissions();
   const isConnected = useConnector((state) => state.status === "connected");
-
-  useEffect(() => {
-    ConnectorModule.start(extra.SERVICE_UUID, extra.CHARACTERISTIC_UUID);
-    console.log("ConnectorModule started.");
-  }, [extra.SERVICE_UUID, extra.CHARACTERISTIC_UUID]);
 
   if (isLoading) {
     return (
